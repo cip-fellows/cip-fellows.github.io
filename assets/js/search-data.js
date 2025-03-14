8f242d531970d0d1912d8e2690492212a599bab6
iAarush <43413800+iAarush@users.noreply.github.com>
2025-03-14 20:09:23 +0000

    Deploying to gh-pages from @ cip-fellows/cip-fellows.github.io@f58d9b48d1dbc96c2b7cea26b79608f2e15dfc93 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,51 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{
+  },{id: "nav-program",
+          title: "Program",
+          description: "About the CIP Fellows Program",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/program/";
+          },
+        },{id: "nav-team",
+          title: "Team",
+          description: "About the CIP Fellows Team, including PIs and Co-PIs.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/team/";
+          },
+        },{id: "nav-fellows",
+          title: "Fellows",
+          description: "About the current CIP Fellows and alumni, along with links to their individual pages",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/fellows/";
+          },
+        },{id: "nav-news-amp-events",
+          title: "News &amp; Events",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news-events/";
+          },
+        },{id: "news-this-is-a-test-announcement",
+          title: 'This is a test announcement.',
+          description: "",
+          section: "News",},{id: "news-this-is-a-test-announcement",
+          title: 'This is a test announcement.',
+          description: "",
+          section: "News",},{id: "projects-alumni-1",
+          title: 'Alumni 1',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/alumni1/";
+            },},{id: "projects-fellow-1",
+          title: 'Fellow 1',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/fellow1/";
+            },},{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
