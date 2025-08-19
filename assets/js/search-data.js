12f13f297c5a878ad819eccdc41971a5d90f1649
iAarush <43413800+iAarush@users.noreply.github.com>
2025-08-19 11:18:34 +0000

    Deploying to gh-pages from @ cip-fellows/cip-fellows.github.io@336df329144fc77933fa16fa84e86d8e67d3b936 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -120,6 +120,11 @@ ninja.data = [{
           description: "",
           section: "Outcomes",handler: () => {
               window.location.href = "/outcomes/accessible-ai-and-hpc-education-for-all/";
+            },},{id: "outcomes-about-an-ai-assisted-mobile-solution-to-reliably-diagnose-obstructive-sleep-apnea-osa",
+          title: 'About an AI-assisted mobile solution to reliably diagnose Obstructive Sleep Apnea (OSA)',
+          description: "",
+          section: "Outcomes",handler: () => {
+              window.location.href = "/outcomes/obstructive-sleep-apnea/";
             },},{id: "projects-anjali-shinde",
           title: 'Anjali Shinde',
           description: "2025, SDSU",
